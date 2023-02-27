8ca1679ae99e408f6c46a39464aeac1392a53e50
Б - 6 < - 6@DESKTOP-OV2L3H7>
2023-02-27 20:25:29 +0300

    Добавьте файлы проекта.

---
+++ Tiunov C++ Модуль номер 5/Source.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+
+// в понедельник практическая по модулям 4 и 5 
+// Модуль номер 5 
+// линеный поиск элементов в массиве
+// linearSearch
+
+// вывод массива в консоль
+template <typename T> 
+void print_arr(T arr[], const int length){
+	std::cout << "[";
+	for (int i = 0; i < length; i++)
+		std::cout << arr[i] << ", ";
+	std::cout << "\b\b]\n";
+		}
+// Линейный поиск в вхождение элемента в массиве
+template <typename T>
+int search_index(T arr[], const int lenght, T value, int begin=0) {
+	for (int i = begin; i < lenght; i++)
+		if (arr[i] == value)
+			return i;
+	return -1;
+}
+// линейный поиск последнего вхождения элемента в массив
+template <typename T>
+int search_last_index(T arr [], const int length, T value) {
+	for (int i= length -1; i >0; i--)
+		if (arr[i] == value)
+			return i; 
+	return -1;
+}
+template <typename T>
+int search_last_index(T arr[], const int length, T value, int begin) {
+	for (int i = begin ; i > 0; i--)
+		if (arr[i] == value)
+			return i;
+	return -1;
+}
+
+
+void subword(char word[], const int lenght, char sym) {
+	int index = search_index(word, lenght, sym);
+	if (index != -1) {
+		for (int i = index; i < lenght; i++)
+			std::cout << word[i];
+		std::cout << std::endl;
+	}
+	else
+		std::cout << "Error:\n";
+
+}
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int n;
+
+	// линейный поиск
+	/*
+	const int size = 10;
+	int arr[size]{ 7, 15, 10, -3, 15, 60, 8, 60, 15,-3};
+		//          0  1   2   3  4   5   6  7    8   9
+	std::cout << "Массив: \n";
+	print_arr(arr, size);
+
+	int index;
+	std::cout << "Введите число --> ";
+	std::cin >> n;
+
+	index= search_last_index(arr, size, n, 5);
+
+	//index = search_index(arr, size, n, 2); старая версия
+	if (index != -1)
+		std::cout << "Индекцс =  " << index << "\n";
+	else
+		std::cout << "Числа нет в массиве.\n";
+	*/
+	
+
+	// Задание под слово
+
+
+	std::cout << "Слово:\n";
+	const int word_size = 8;
+	char word[word_size]{
+		'f','u','n','c','t','i','o','n'
+	};
+	for (int i = 0; i < word_size; i++)
+		std::cout << word[i];
+	std::cout << "\nВведите симвл ->";
+	char sym;
+	std::cin >> sym;
+	std::cout << "Подслово:\n";
+	subword(word, word_size, sym);
+
+	
+	
+	return 0;
+	}
+
+
+
+
+
+ 
+
+
